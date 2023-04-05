df356a72269ba8c35deff43b604e381d33cd24f7
Israel <isra_quirozz@hotmail.com>
2023-04-05 23:53:59 +0200

    Complete desktop view

--- index.js
+++ index.js
@@ -1,108 +1,159 @@
 // NAVBAR
-const openMenu = document.querySelector(".openMenu");
-const closeMenu = document.querySelector(".closeMenu");
-const menuItems = document.querySelector(".menuItems");
+// const openMenu = document.querySelector(".openMenu");
+// const closeMenu = document.querySelector(".closeMenu");
+// const menuItems = document.querySelector(".menuItems");
 
-openMenu.addEventListener("click", () => {
-    openMenu.classList.toggle("hidden");
-    closeMenu.classList.toggle("visible");
-    menuItems.classList.toggle("visible_nav");
+// openMenu.addEventListener("click", () => {
+//     openMenu.classList.toggle("hidden");
+//     closeMenu.classList.toggle("visible");
+//     menuItems.classList.toggle("visible_nav");
+// });
+
+// closeMenu.addEventListener("click", () => {
+//     closeMenu.classList.toggle("visible");
+//     openMenu.classList.toggle("hidden");
+//     menuItems.classList.toggle("visible_nav");
+// });
+
+// ABOUT ME
+const aboutLinks = document.querySelectorAll(".about-link");
+const profileLink = document.querySelector(".profile");
+const educationLink = document.querySelector(".education");
+const skillsLink = document.querySelector(".skills");
+const profileLeft = document.querySelector(".profile-left");
+const profileRight = document.querySelector(".profile-right");
+const educationLeft = document.querySelector(".education-left");
+const educationRight = document.querySelector(".education-right");
+const skillsLeft = document.querySelector(".skills-left");
+const skillsRight = document.querySelector(".skills-right");
+const skillDesc = document.querySelector(".skill-description");
+
+function activeLink() {
+  aboutLinks.forEach((link) => {
+    link.classList.remove("about-link-active");
+    this.classList.add("about-link-active");
+  });
+}
+
+aboutLinks.forEach((link) => {
+  link.addEventListener("click", activeLink);
 });
 
-closeMenu.addEventListener("click", () => {
-    closeMenu.classList.toggle("visible");
-    openMenu.classList.toggle("hidden");
-    menuItems.classList.toggle("visible_nav");
+// PROFILE
+profileLink.onclick = () => {
+  profileLeft.classList.remove("hide");
+  profileRight.classList.remove("hide");
+  educationLeft.classList.add("hide");
+  educationRight.classList.add("hide");
+  skillsLeft.classList.add("hide");
+  skillsRight.classList.add("hide");
+  skillDesc.classList.add("hide");
+};
+
+// EDUCATION
+educationLink.onclick = () => {
+  educationLeft.classList.remove("hide");
+  educationRight.classList.remove("hide");
+  profileLeft.classList.add("hide");
+  profileRight.classList.add("hide");
+  skillsLeft.classList.add("hide");
+  skillsRight.classList.add("hide");
+  skillDesc.classList.add("hide");
+};
+
+// SKILLS
+skillsLink.onclick = () => {
+  skillsLeft.classList.remove("hide");
+  skillsRight.classList.remove("hide");
+  skillDesc.classList.remove("hide");
+  educationLeft.classList.add("hide");
+  educationRight.classList.add("hide");
+  profileLeft.classList.add("hide");
+  profileRight.classList.add("hide");
+};
+
+// EDUCATION
+const coderHouse = document.querySelector(".coder-house");
+const coderHouseInfo = document.querySelector(".coderHouse-info");
+const udemy = document.querySelector(".udemy");
+const udemyInfo = document.querySelector(".udemy-info");
+
+coderHouse.addEventListener("click", () => {
+  coderHouseInfo.classList.toggle("hide");
+  udemyInfo.classList.add("hide");
+});
+udemy.addEventListener("click", () => {
+  udemyInfo.classList.toggle("hide");
+  coderHouseInfo.classList.add("hide");
 });
 
-// CIRCLE PROGRESS BAR
-// let circularProgress = document.querySelector(".circular-progress"),
-//     progressValue = document.querySelector(".progress-value");
+// SKILLS HTML
+const skills = [
+  {
+    skill: "HTML",
+    percentage: 90,
+  },
+  {
+    skill: "CSS",
+    percentage: 90,
+  },
+  {
+    skill: "JAVASCRIPT",
+    percentage: 80,
+  },
+  {
+    skill: "FIGMA",
+    percentage: 60,
+  },
+  {
+    skill: "PYTHON",
+    percentage: 80,
+  },
+  {
+    skill: "REACT",
+    percentage: 70,
+  },
+  {
+    skill: "NODEJS",
+    percentage: 40,
+  },
+  {
+    skill: "MONGODB",
+    percentage: 40,
+  },
+];
 
-// let progressStartValue = 0,
-//     progressEndValue = 90,
-//     speed = 10;
+const skillsContainer = document.querySelector(".tech-skills-bars");
 
-// let progress = setInterval(() => {
-//     progressStartValue++;
+function skillHTML(skills) {
+  skills.forEach((skill) => {
+    const techSkill = document.createElement("div");
+    techSkill.className = "tech-skill";
+    techSkill.innerHTML = `
+            <p>${skill.skill}</p>
+            <div class="skill-bar">
+                <div class="skill-bar-in ${skill.skill.toLowerCase()}-bar"></div>
+            </div>
+        `;
+    skillsContainer.appendChild(techSkill);
+  });
+}
+skillHTML(skills);
 
-//     progressValue.textContent = `${progressStartValue}%`;
-//     circularProgress.style.background = `conic-gradient(#32e0c4 ${
-//   progressStartValue * 3.6
-// }deg, rgba(255, 255, 255, 0.099) 0deg)`;
-
-//     if (progressStartValue == progressEndValue) {
-//         clearInterval(progress);
-//     }
-//     console.log(progressStartValue);
-// }, speed);
-
-// ABOUT
-
-// MENU
-const profile = document.getElementById("profile");
-const education = document.getElementById("education");
-const skills = document.getElementById("skills");
-
-// LEFT
-const profileLeft = document.querySelector(".profileImg");
-const educationSkills = document.querySelector(".about_left__container");
-const educationLeft = document.querySelector(".education_leftContent");
-const skillsLeft = document.querySelector(".skill_leftContent");
-const educationMarginContainer = document.querySelector(
-    ".about_left__container"
-);
-
-// RIGHT
-const profileRight = document.querySelector(".profileContent");
-const educationRight = document.querySelector(".educationContent");
-const skillsRight = document.querySelector(".skillsContent");
-
-// EDUCATION LINK
-education.addEventListener("click", (e) => {
-    e.preventDefault();
-    profile.classList.remove("active");
-    education.classList.add("active");
-    skills.classList.remove("active");
-    profileLeft.classList.add("about_hidden");
-    skillsLeft.classList.add("about_hidden");
-    educationSkills.classList.remove("about_hidden");
-    educationLeft.classList.remove("about_hidden");
-    profileRight.classList.add("about_hidden");
-    skillsRight.classList.add("about_hidden");
-    educationRight.classList.remove("about_hidden");
-    educationMarginContainer.classList.add("about_left__containerMargin");
-    educationSkills.classList.remove("about_hidden_mobile");
-});
-
-// SKILLS LINK
-skills.addEventListener("click", (e) => {
-    e.preventDefault();
-    profile.classList.remove("active");
-    education.classList.remove("active");
-    skills.classList.add("active");
-    profileLeft.classList.add("about_hidden");
-    educationSkills.classList.remove("about_hidden");
-    educationLeft.classList.add("about_hidden");
-    skillsLeft.classList.remove("about_hidden");
-    profileRight.classList.add("about_hidden");
-    educationRight.classList.add("about_hidden");
-    skillsRight.classList.remove("about_hidden");
-    educationMarginContainer.classList.remove("about_left__containerMargin");
-    educationSkills.classList.add("about_hidden_mobile");
-    // skillsRight.classList.add("about_left__containerMargin");
-});
-
-// PROFILE LINK
-
-profile.addEventListener("click", (e) => {
-    e.preventDefault();
-    education.classList.remove("active");
-    skills.classList.remove("active");
-    profile.classList.add("active");
-    educationSkills.classList.add("about_hidden");
-    profileLeft.classList.remove("about_hidden");
-    educationRight.classList.add("about_hidden");
-    skillsRight.classList.add("about_hidden");
-    profileRight.classList.remove("about_hidden");
+// SWIPER
+let swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
